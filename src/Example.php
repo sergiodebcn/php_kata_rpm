3d99ec8541328b145c6b5c42712b6275ff49a37b
Sergio Baños <sergio.banos@atrapalo.com>
2017-02-04 20:45:49 +0100

    first_commit

---
+++ src/Example.php
@@ -0,0 +1,16 @@
+<?php
+namespace Kata;
+
+
+class Example
+{
+    /**
+     * @param int $first
+     * @param int $second
+     * @return int
+     */
+    public function add($first, $second)
+    {
+        return (int) $first + (int) $second;
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Kata\Tests;
+
+use Kata\Example;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @dataProvider validSumsProvider
+     * @param int $a
+     * @param int $b
+     * @param $expectedResult
+     */
+    public function validSums($a, $b, $expectedResult)
+    {
+        $this->assertEquals(
+            $expectedResult,
+            (new Example())->add($a, $b)
+        );
+    }
+
+    public function validSumsProvider()
+    {
+        return [
+            [null, null, 0],
+            [0, 0, 0],
+            [1, 1, 2]
+        ];
+    }
+}
